85b26361dd388ffe8151f524ec719ddfa48e1a21
Usage_USAGI <chlorine3p5@gmail.com>
2020-02-01 19:16:26 +0900

    added elif

--- discordbot.py
+++ discordbot.py
@@ -19,16 +19,16 @@ async def on_message(message):
     if message.author.bot:
         return
     
-    if re.search("[ねむい|眠い|眠たい]", message.content):
+    elif re.search("[ねむい|眠い|眠たい]", message.content):
         await message.channel.send(f"{message.author.mention}さん 寝ましょう")
 
-    if re.search("おはよ", message.content):
+    elif re.search("おはよ", message.content):
         await message.channel.send(f"{message.author.mention}さん おはよう")
     
-    if re.search("[こんばんは|こんばんわ]", message.content):
+    elif re.search("[こんばんは|こんばんわ]", message.content):
         await message.channel.send(f"{message.author.mention}さん こんばんは")
     
-    if re.search("[こんにちは|こんにちわ]", message.content):
+    elif re.search("[こんにちは|こんにちわ]", message.content):
         await message.channel.send(f"{message.author.mention}さん こんにちは")
     
 
